833dc3b13734991cb100503a6db234a5fe43d63c
谢海鸿 <xinlle.01@163.com>
2018-08-16 10:57:49 +0800

    1

--- common/mf/mf-common.js
+++ common/mf/mf-common.js
@@ -6,17 +6,52 @@ var mf = mf || {};
  * jsonFileName
  */
 mf.util = (function () {
-    var jsonFileName = function (url) {        
+    var jsonFileName = function (url) {
         var jsonFileName = url;
         var testMode = localStorage.testMode === '1' ? true : false;
         if (testMode) {
             var strs = url.replace('.do', '').replace('.action', '').split('/');
-            jsonFileName =  strs.length > 1 ? strs[strs.length - 2] + '_' + strs[strs.length - 1] : strs[0];
+            jsonFileName = strs.length > 1 ? strs[strs.length - 2] + '_' + strs[strs.length - 1] : strs[0];
             jsonFileName = '../../common/testData/' + jsonFileName + '.json';
         }
         return jsonFileName;
     };
+
     return {
         jsonFileName: jsonFileName
-    }
+    };
+})();
+
+/**
+ * 用于组件封装, vue app列表类，小块类等很有用
+ */
+mf.htmlToObj = (function () {
+    var obj = {};
+    var init = function (html) {
+        obj = $(html);
+    };
+    var replace = function (selector, html) {
+        var parent = obj.find(selector).parent();
+        obj = parent.empty().append(html);
+        return obj;
+    };
+
+    var append = function (selector, html) {
+        obj = obj.find(selector).append(html);
+        return obj;
+    };
+    var remove = function(selector){
+        obj = obj.fid(selector).remove();
+        return obj;
+    };
+    var toString = function(){
+        return obj.get(0).outerHTML;
+    };
+    return {
+        init: init,
+        replace: replace,
+        append: append,
+        remove: remove,
+        toString: toString
+    };
 })();
