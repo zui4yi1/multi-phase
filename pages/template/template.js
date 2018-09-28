625d253fff9fb25b3c2d9506b193a951cef9b5d0
谢海鸿 <xinlle.01@163.com>
2018-09-28 15:13:10 +0800

    1

--- pages/template/template.js
+++ pages/template/template.js
@@ -1,21 +1,6 @@
 
 $(function () {
 
-    var tpl = listTemplate.slice(0);
-    tpl.splice(5, 1, '<span>{{item.aplyDate}}</span>');
-    tpl.splice(8, 1, '<span>{{item.statusNm}}</span>');
-    tpl.splice(12, 1, '<div>{{item.reason}}</div>');
-    tpl.splice(15, 1, '<button v-if="item.status==\'03\'" v-on:click.stop="submitAply(item)">提交</button><button v-if="item.status==\'02\'" v-on:click.stop="cancelAply(item)">取消</button>');
-
-
-    var tpl2 = new mf.template(listTemplate);
-    tpl2.replace(5, '<span>{{item.aplyDate}}</span>');
-    tpl2.replace(8, '<span>{{item.statusNm}}</span>');
-    tpl2.replace(12, '<div>{{item.reason}}</div>');
-    tpl2.replace(15, '<button v-if="item.status==\'03\'" v-on:click.stop="submitAply(item)">提交</button><button v-if="item.status==\'02\'" v-on:click.stop="cancelAply(item)">取消</button>');
-
-    // 直接用索引的方式arr[i]=''替换也是可以的
-
     new Vue({
         el: '#app',
         data: function () {
@@ -27,18 +12,29 @@ $(function () {
             'holiday-list': {
                 props: ['items'],
                 //   template: tpl.join('').replace(/\s+/g, ' '),
-                template: tpl2.toString(),
-                methods: {
-                    submitAply: function (item) {
-                        console.info(item)
-                    },
-                    cancelAply: function (item) {
-                        console.info(item)
-                    },
-                    viewAply: function (item) {
-                        console.info(item)
-                    }
-                }
+                //  template: tpl2.toString(),
+                template: [
+                    '<div class="listWrap">',
+                    '   <div class="itemList" v-for="(item, inx) in items">',
+                    '       <div class="itemWrap" >',
+                    '           <div class="item-header">',
+                    '               <div class="header-left">',
+                    '                   <slot name="headLeft" :item="item" ></slot>',
+                    '               </div>',
+                    '               <div class="header-right">',
+                    '                   <slot name="headRight" :item="item" ></slot>',
+                    '               </div>',
+                    '           </div>',
+                    '           <div class="item-body">',
+                    '                <slot name="itemBody" :item="item" ></slot>',
+                    '           </div>',
+                    '           <div class="item-footer">',
+                    '                <slot name="itemFooter" :item="item" ></slot>',
+                    '           </div>',
+                    '       </div>',
+                    '   </div>',
+                    '</div>'].join('').replace(/\s+</g, '<') //replace, 去掉对齐用的空白符
+                   ,methods:{} //组件自带公共方法于此定义
             }
         },
         mounted: function () {
@@ -49,6 +45,17 @@ $(function () {
             mf.ajax('holidays.action', condititon, function (data) {
                 _this.items = data.holidaysList;
             });
+        },
+        methods: {
+            submitAply: function (item) {
+                console.info(item)
+            },
+            cancelAply: function (item) {
+                console.info(item)
+            },
+            viewAply: function (item) {
+                console.info(item)
+            }
         }
     });
 });
