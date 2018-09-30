ff32c556109e172e3c3c497826a7dba512fbe3cb
谢海鸿 <xinlle.01@163.com>
2018-09-30 09:16:24 +0800

    1

--- pages/msgbox/msgbox.js
+++ pages/msgbox/msgbox.js
@@ -20,31 +20,6 @@ $(function () {
             console.info('well done!');
         });
     });
-
-    // 更进一步的封装，可以把id一起做为参数，这样就可以把点击事件一起包含在内，不过最好不要这样做，
-    // 因为绑定dom的事件不是插件本身的功能
-    
-    /*     mf.msgbox.succ = function (selector, content, callback, ops) {
-            $(selector).click(function () {
-                new $.Zebra_Dialog(content || '操作成功', $.extend({
-                    title: '提示',
-                    type: 'confirmation',
-                    overlay_opacity: 0.3,
-                    buttons: [
-                        {
-                            caption: 'OK',
-                            callback: function () {
-                                if (callback instanceof Function)
-                                    callback();
-                                return true;
-                            }
-                        }
-                    ]
-                }, ops));
-            });
-        };
-        mf.msgbox.succ('#succ');  */
-
 });
 
 
